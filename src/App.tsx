48eb31c38183e38627d722132e588cb7f983b0a8
Alexander Spatar <spatar@teleofis.ru>
2023-08-31 09:51:04 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { Suspense } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import "./styles/index.scss";
+
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames";
+
+export enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div
+      className={classNames("app", {}, [theme])}
+    >
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+import path from "path";
+import webpack from "webpack";
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
+import { BuildEnv, BuildOptions } from "./config/build/types/config";
+
+export default (env: BuildEnv) => {
+  const paths: BuildOptions["paths"] = {
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+  };
+
+  const mode: BuildOptions["mode"] = env.mode || "development";
+  const port: BuildOptions["port"] = env.port || 3000;
+
+  const isDev: BuildOptions["isDev"] = mode === "development";
+
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths, 
+    isDev,
+    port,
+  });
+
+  return config;
+};
